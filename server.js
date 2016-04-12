828e2e04059d98c66c41dd67d1697dc8116850c9
Kavit Shah <kavit.shah@citrix.com>
2016-04-11 17:25:22 -0700

    updates server.js

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
+`use strict`;
 
-var express = require('express');
+var express = require(`express`);
 var app = express();
 
-var PORT = 3000;
+const PORT = 3000;
 
-app.use(`/`, express.static(__dirname + '/public/'));
+app.use(`/`, express.static(`${__dirname}/public/`));
 
 app.listen(PORT, () => {
   console.log(`Server started: http://localhost: ${PORT}/`);
