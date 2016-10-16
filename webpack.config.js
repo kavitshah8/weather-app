35fffd76e33b35e01097476567ab1676ea2c5efe
Kavit Shah <kavit.shah@citrix.com>
2016-10-16 11:22:18 -0700

    enables hot module replacement with webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,38 @@
 const validator = require('webpack-validator');
 const path = require('path');
+var webpack = require('webpack');
 
 const config = {
 
-	entry: path.join( __dirname, 'src/Router.jsx'),
-
+	entry: [
+	    'webpack-dev-server/client?https://localhost:3443',
+        'webpack/hot/dev-server',
+		path.join( __dirname, 'src/Router.jsx')
+	],
 	output: {
 		path: path.join( __dirname, 'public/assets/js'),
-		filename: 'app.js'
+		filename: 'app.js',
+		publicPath: 'assets/js/',		
 	},
 
 	module: {
 		loaders: [
 			{
-				test: /\.js|jsx$/, loader: 'babel',
+				test: /\.js|jsx$/, loaders: ['react-hot', 'babel'],
 				exclude: /node_modules/
 			},
 			{
 				test: /\.css$/, loaders: ['style', 'css']
 			}
 		]
+	},
+	plugins: [
+		new webpack.HotModuleReplacementPlugin()
+	],
+	devServer: {
+		inline: true,
+		hot: true,
+		contentBase: './public'
 	}
 };
 
