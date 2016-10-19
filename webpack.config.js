7debdfe09746cc57a0487a59ffd41d81f9aff10f
Kavit Shah <kavitshah8@gmail.com>
2016-10-19 08:48:48 -0700

    configure wds with node API

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,19 @@
 const validator = require('webpack-validator');
 const path = require('path');
-var webpack = require('webpack');
+const webpack = require('webpack');
 
 const config = {
 
 	entry: [
-	    'webpack-dev-server/client?https://localhost:3443',
+	    'webpack-dev-server/client?http://localhost:3443',
         'webpack/hot/dev-server',
 		path.join( __dirname, 'src/Router.jsx')
 	],
 	output: {
-		path: path.join( __dirname, 'public/assets/js'),
-		filename: 'app.js',
-		publicPath: 'assets/js/',		
+		path: path.join( __dirname, 'public/assets/js/'),
+		filename: 'build.js',
+		publicPath: '',		
 	},
-
 	module: {
 		loaders: [
 			{
@@ -28,12 +27,7 @@ const config = {
 	},
 	plugins: [
 		new webpack.HotModuleReplacementPlugin()
-	],
-	devServer: {
-		inline: true,
-		hot: true,
-		contentBase: './public'
-	}
+	]
 };
 
 module.exports = validator(config);
